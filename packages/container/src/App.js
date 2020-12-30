6760e9eb2168fc747cf30ff919411ac888e6815b
Rakesh Thakur <mailtorkthakur@gmail.com>
2020-12-30 12:23:43 +0530

    Added style generator

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+const generateClassName = createGenerateClassName({
+    productionPrifix: 'con',
+});
 
 
 export default() => {
     return( 
+        <StylesProvider generateClassName={generateClassName}>
+   
         <BrowserRouter>
             <div>
                 <Header />
             <MarketingApp />
             </div>
         </BrowserRouter>
+        </StylesProvider>
     );
 };
